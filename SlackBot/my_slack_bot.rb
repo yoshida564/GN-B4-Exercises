b69e5e594934d970fef19204d3b2f132e5108fde
Yoshinari Nomura <nom@nomlab.org>
2015-03-16 15:31:28 +0900

    Add main

--- SlackBot/my_slack_bot.rb
+++ SlackBot/my_slack_bot.rb
@@ -1,8 +1,20 @@
 #! /usr/bin/env ruby
 # -*- coding: utf-8 -*-
-require 'slack_bot'
 
-#---------- MySlackBot ----------
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'slack_bot'
+require 'pp'
+
 class MySlackBot < SlackBot
-  # 機能を追加
+  # Add your original functions
+end
+
+
+## sample main
+
+slack = MySlackBot.new("setting.yml")
+
+slack.get_message.each do |msg|
+  pp msg
 end
