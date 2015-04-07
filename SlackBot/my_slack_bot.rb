e9e4a6a3b8e019e7474dd73104305d71cc928517
Hisashi Yoshida <yoshida-h@swlab.cs.okayama-u.ac.jp>
2015-04-07 16:03:17 +0900

    Create repeat method

--- SlackBot/my_slack_bot.rb
+++ SlackBot/my_slack_bot.rb
@@ -8,6 +8,21 @@ require 'pp'
 
 class MySlackBot < SlackBot
   # Add your original functions
+  
+  def repeat(message)
+    m1 = /「/.match(message)
+    if m1 != nil then
+      m2 = m1.post_match
+      m3 = /」と言って/.match(m2)
+      if m3 != nil then
+        m4 = m3.pre_match
+        p m4+" by bot"
+        post_message(m4+" by bot")
+        repeat(m3.post_match)
+      end
+    end
+  end
+
 end
 
 
@@ -15,6 +30,7 @@ end
 
 slack = MySlackBot.new("setting.yml")
 
-slack.get_message.each do |msg|
-  pp msg
+slack.get_message["messages"].each do |msg|
+  slack.repeat(msg["text"])
+  #pp msg["text"]
 end
